16a8b921b7508979abf4cff5ef25c6e0b61f9cc8
tomixy <92695929+tetracalibers@users.noreply.github.com>
2025-03-28 13:34:05 +0900

    astro.config.mjsを再現

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,47 @@ import { fileURLToPath } from "node:url"
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
+import rehypePrettyCode from "rehype-pretty-code"
+import { addColorPreview } from "./plugins/pretty-code/add-color-preview"
+import { transformerNotationErrorLevel } from "@shikijs/transformers"
+
+import remarkBreaks from "remark-breaks"
+import remarkMath from "remark-math"
+import rehypeKatex from "rehype-katex"
+
+/** @type {import("rehype-pretty-code").Options} */
+const prettyCodeOptions = {
+  theme: {
+    dark: "synthwave-84",
+    light: "snazzy-light"
+  },
+  defaultLang: "plaintext",
+  transformers: [transformerNotationErrorLevel()],
+  onVisitLine(element) {
+    addColorPreview(element)
+  }
+}
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://tomixyz-biography.com",
+  base: "",
+  trailingSlash: "ignore",
+  scopedStyleStrategy: "where",
+  devToolbar: {
+    enabled: false
+  },
+  server: {
+    port: 3000
+  },
+  markdown: {
+    syntaxHighlight: false, // Disable syntax built-in syntax hightlighting from astro
+    rehypePlugins: [
+      [rehypeKatex, {}],
+      [rehypePrettyCode, prettyCodeOptions]
+    ],
+    remarkPlugins: [remarkMath, remarkBreaks]
+  },
   integrations: [svelte(), mdx()],
   vite: {
     resolve: {
