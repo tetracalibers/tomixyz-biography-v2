a92691bf12521c6aa49e93da608f2f67f85f6d4d
tomixy <92695929+tetracalibers@users.noreply.github.com>
2025-03-31 11:08:30 +0900

    projectコレクションを作成

--- src/content.config.ts
+++ src/content.config.ts
@@ -15,4 +15,24 @@ const page = defineCollection({
   })
 })
 
-export const collections = { page }
+const project = defineCollection({
+  loader: glob({ pattern: "**/*.mdx", base: "./src/content/project" }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string().max(100, "The title length must be less than or equal to 100 chars"),
+      description: z.string(),
+      meta: z
+        .object({
+          title: z.string().optional(),
+          description: z.string().optional()
+        })
+        .optional(),
+      tags: z.array(z.string()).default([]),
+      date: z.string(),
+      image: image(),
+      url: z.string().url().optional(),
+      github: z.string().url().optional()
+    })
+})
+
+export const collections = { page, project }
