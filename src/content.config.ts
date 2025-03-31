d424c7288e2339f9d7d052abfe01b5ff7f04bb8a
tomixy <92695929+tetracalibers@users.noreply.github.com>
2025-03-31 11:05:05 +0900

    コンテンツコレクションの定義ファイルをv5仕様に

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,8 @@
 import { defineCollection, z } from "astro:content"
+import { glob } from "astro/loaders"
 
 const page = defineCollection({
+  loader: glob({ pattern: "**/*.mdx", base: "./src/content/page" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
