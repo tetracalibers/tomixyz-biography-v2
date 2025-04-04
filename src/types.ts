27f4cc62eb4f2539af6fef8273253a2194462769
tomixy <92695929+tetracalibers@users.noreply.github.com>
2025-04-04 14:40:50 +0900

    writingコレクションからデータを取得して描画するよう変更

--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,7 @@ export type NavItem = {
   path: string
   title: string
 }
+
+export type ArrayElement<ArrayType extends readonly unknown[]> = ArrayType extends readonly (infer ElementType)[]
+  ? ElementType
+  : never
