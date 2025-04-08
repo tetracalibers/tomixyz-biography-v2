cdd301c71dfb6fbdfdf669714ca2c53531666127
tomixy <92695929+tetracalibers@users.noreply.github.com>
2025-04-08 15:37:38 +0900

    compareDateForSortはlib/sort.tsに移動

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,7 +1,3 @@
 const MONTHS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
 export const getMonthName = (date: string | Date) => MONTHS[new Date(date).getMonth()]
-
-export const compareDateForSort = <T extends { data: { date: string } }>(a: T, b: T) => {
-  return +new Date(b.data.date) - +new Date(a.data.date)
-}
