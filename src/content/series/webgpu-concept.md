40a42cf3aac70a98b639481385f92de8746dd674
tomixy <92695929+tetracalibers@users.noreply.github.com>
2025-04-09 08:57:58 +0900

    シリーズ記事の目次がとりあえず動くところまで

--- src/content/series/webgpu-concept.md
+++ src/content/series/webgpu-concept.md
@@ -3,3 +3,5 @@ title: WebGPUのコンセプト
 articles:
   - webgpu-concept-from-history
 ---
+
+WebGPUのコンセプトと構成要素について、WebGLと比較しながら解説するシリーズ
---
+++ src/lib/series.ts
@@ -0,0 +1,42 @@
+import type { AstroComponentFactory } from "astro/runtime/server/index.js"
+import { getEntries, getEntry, render, type CollectionEntry } from "astro:content"
+
+type SeriesArticle = CollectionEntry<"recipe">
+type SeriesArticleSlug = SeriesArticle["id"]
+
+export interface SeriesArticles {
+  seriesId: string
+  slugs: SeriesArticleSlug[]
+  articles: SeriesArticle[]
+  isFirstArticle: (slug: SeriesArticleSlug) => boolean
+  getNextArticles: (slug: SeriesArticleSlug) => SeriesArticle[]
+  Summary: AstroComponentFactory
+}
+
+export const collectSeriesArticles = async (seriesId: string): Promise<SeriesArticles> => {
+  const series = await getEntry("series", seriesId)
+
+  if (!series) {
+    throw new Error(`series ${seriesId} not found`)
+  }
+
+  const { Content } = await render(series)
+
+  const seriesArticles = series.data.articles.map((article) => ({ collection: article.collection, id: article.id }))
+  const articleEntries = await getEntries(seriesArticles)
+
+  const slugs = seriesArticles.map((article) => article.id)
+
+  const findIndex = (slug: SeriesArticleSlug) => slugs.indexOf(slug)
+  const isFirstArticle = (slug: SeriesArticleSlug) => findIndex(slug) === 0
+  const getNextArticles = (slug: SeriesArticleSlug) => articleEntries.slice(findIndex(slug) + 1)
+
+  return {
+    seriesId,
+    slugs,
+    articles: articleEntries,
+    isFirstArticle,
+    getNextArticles,
+    Summary: Content
+  }
+}
