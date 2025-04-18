85582f43728b804696bdcc6689aae29571f60888
tomixy <92695929+tetracalibers@users.noreply.github.com>
2025-04-18 19:21:36 +0900

    プレビューブランチではdraftのページもビルドするように

--- src/lib/filter.ts
+++ src/lib/filter.ts
@@ -4,5 +4,6 @@ export const and = <T>(...filters: ((entry: T) => boolean)[]) => {
 
 export const filterPublic = (entry: { data: { draft: boolean } }) => {
   if (import.meta.env.DEV) return true
+  if (import.meta.env.CF_PAGES_BRANCH !== "main") return true
   return !entry.data.draft
 }
